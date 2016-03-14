c77629f894ce32a3bc128c822b6df13081b8d1b9
Dario Kriskovic <dario.kriskovic@gmail.com>
2016-03-14 15:31:53 +0100

    added nunjucks template engine

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var useref = require('gulp-useref');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var autoprefixer = require('gulp-autoprefixer');
-
+var nunjucksRender = require('gulp-nunjucks-render');
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
@@ -40,4 +40,22 @@ gulp.task('useref', function(){
     .pipe(useref())
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
+});
+
+// gulp.task('nunjucks', function() {
+//   nunjucksRender.nunjucks.configure(['app/templates/'], {watch:false});
+//
+//   return gulp.src('app/pages/**/*.+(html|nunjucks)')
+//   // Renders template with nunjucks
+//   .pipe(nunjucksRender())
+//   // output files in app folder
+//   .pipe(gulp.dest('app'))
+// });
+
+gulp.task('nunjucks', function() {
+  return gulp.src('app/pages/*.nunjucks')
+  .pipe(nunjucksRender({
+  path: ['app/pages/', 'app/templates/'] // String or Array
+  }))
+  .pipe(gulp.dest('app'));
 });
